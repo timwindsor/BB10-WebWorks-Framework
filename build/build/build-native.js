9c0b96a9fb7b1afa2083e476096960051bcf9005
Adrian Lee <adrilee@rim.com>
2012-05-20 16:11:42 -0400

    Fixes blackberry/BB10-WebWorks-Framework#98: Framework jake build does not report proper result

--- build/build/build-native.js
+++ build/build/build-native.js
@@ -19,9 +19,48 @@ var childProcess = require("child_process"),
     path = require("path"),
     _c = require("./conf");
 
-function _getCmd() {
-    var exts = fs.readdirSync(_c.EXT), ext,
-        cmd = "",
+function _exec(cmd, callback) {
+    childProcess.exec(cmd, function (error, stdout, stderr) {
+        if (error) {
+            console.log(stdout);
+            console.log(stderr);
+            callback(false);
+        } else {
+            callback(true);
+        }
+    });
+}
+
+function _spawn(cmd, cwd, callback) {
+    var cmd_split = cmd.split(" "),
+        array = [],
+        sh, i;
+
+    for (i = 1; i <= cmd_split.length - 1; i++) {
+        array.push(cmd_split[i]);
+    }
+
+    sh = childProcess.spawn(cmd_split[0], array, { 'cwd': cwd });
+
+    sh.stdout.on('data', function (data) {
+        console.log('stdout: ' + data);
+    });
+
+    sh.stderr.on('data', function (data) {
+        console.log('stderr: ' + data);
+    });
+
+    sh.on('exit', function (code) {
+        if (code !== 0) {
+            callback(false, "Error " + code + " when excuting: " + cwd + " : " + cmd);
+        } else {
+            callback(true);
+        }
+    });
+}
+
+function _buildNative(ext, next, callback) {
+    var cmd,
         nativeDir, simDir, deviceDir,
         configureX86, configureARM,
         stripX86, stripARM,
@@ -33,69 +72,143 @@ function _getCmd() {
         CP_CMD = "cp ",
         SH_CMD = "sh ";
         
-    for (ext in exts) {
-        //Native build directories
-        nativeDir = path.join(_c.EXT, exts[ext], "native");
-        simDir = path.join(_c.EXT, exts[ext], "simulator");
-        deviceDir = path.join(_c.EXT, exts[ext], "device");
-        
-        //configure-qsk commands
-        configureX86 = path.join(simDir, "configure-qsk x86");
-        configureARM = path.join(deviceDir, "configure-qsk arm a9");
+    //Native build directories
+    nativeDir = path.join(_c.EXT, ext, "native");
+    simDir = path.join(_c.EXT, ext, "simulator");
+    deviceDir = path.join(_c.EXT, ext, "device");
     
-        //strip binary commands
-        stripX86 = "ntox86-strip *.so";
-        stripARM = "ntoarmv7-strip *.so";
-          
-        //If native folder exists, Build
-        if (path.existsSync(nativeDir)) {
-            if (!path.existsSync(simDir)) {
-                fs.mkdirSync(simDir);
-            }
+    //configure-qsk commands
+    configureX86 = path.join(simDir, "configure-qsk x86");
+    configureARM = path.join(deviceDir, "configure-qsk arm a9");
 
-            if (!path.existsSync(deviceDir)) {
-                fs.mkdirSync(deviceDir);
-            }
-            
-            if (cmd) {
-                cmd += AND_CMD;
-            }
-            
-            if (utils.isWindows()) {
-                cmd += CP_CMD + _c.DEPENDENCIES_CONFIGURE_QSK + " " +
-                simDir + AND_CMD + CP_CMD + _c.DEPENDENCIES_CONFIGURE_QSK +
-                " " + deviceDir + AND_CMD +
-                CD_CMD + simDir + AND_CMD + 
-                SH_CMD + configureX86 + AND_CMD +
-                MAKE_CMD + AND_CMD + stripX86 + AND_CMD + 
-                CD_CMD + deviceDir + AND_CMD + 
-                SH_CMD + configureARM + AND_CMD +
-                MAKE_CMD + AND_CMD + stripARM;
-            } else {
-                cmd += CP_CMD + _c.DEPENDENCIES_CONFIGURE_QSK + " " +
-                simDir + AND_CMD + CP_CMD + _c.DEPENDENCIES_CONFIGURE_QSK +
-                " " + deviceDir + AND_CMD +
-                CD_CMD + simDir + AND_CMD + 
-                configureX86 + AND_CMD +
-                MAKE_CMD + AND_CMD + stripX86 + AND_CMD +
-                CD_CMD + deviceDir + AND_CMD + 
-                configureARM + AND_CMD +
-                MAKE_CMD + AND_CMD + stripARM;
-            }
-            
+    //strip binary commands
+    stripX86 = "ntox86-strip *.so";
+    stripARM = "ntoarmv7-strip *.so";
+      
+    //If native folder exists, Build
+    if (path.existsSync(nativeDir)) {
+        console.log("Building native ext: " + ext);
+        
+        if (!path.existsSync(simDir)) {
+            fs.mkdirSync(simDir);
         }
+
+        if (!path.existsSync(deviceDir)) {
+            fs.mkdirSync(deviceDir);
+        }
+        
+        if (utils.isWindows()) {
+            cmd = CP_CMD + _c.DEPENDENCIES_CONFIGURE_QSK + " " +
+                simDir + AND_CMD + CP_CMD + _c.DEPENDENCIES_CONFIGURE_QSK +
+                " " + deviceDir + AND_CMD +
+                CD_CMD + simDir + AND_CMD +
+                SH_CMD + configureX86 + AND_CMD +
+                CD_CMD + deviceDir + AND_CMD +
+                SH_CMD + configureARM;
+            
+            // Copy and run configure-qsk
+            _exec(cmd, function (success) {
+                if (!success) {
+                    callback(false, "Failed executing command: " + cmd);
+                }
+                
+                // Make for Simulator
+                _spawn(MAKE_CMD, simDir, function (success, err) {
+                    if (!success) {
+                        callback(false, err);
+                    }
+
+                    // Make for Device
+                    _spawn(MAKE_CMD, deviceDir, function (success, err) {
+                        if (!success) {
+                            callback(false, err);
+                        }
+                    
+                        cmd = CD_CMD + simDir + AND_CMD + stripX86 + AND_CMD +
+                            CD_CMD + deviceDir + AND_CMD + stripARM;
+                        
+                        // Strip .so files
+                        _exec(cmd, function (success) {
+                            if (!success) {
+                                callback(false, "Failed executing command: " + cmd);
+                            }
+                            
+                            next();
+                        });
+                    });
+                });
+            });
+        } else {
+            cmd = CP_CMD + _c.DEPENDENCIES_CONFIGURE_QSK + " " +
+                simDir + AND_CMD + CP_CMD + _c.DEPENDENCIES_CONFIGURE_QSK +
+                " " + deviceDir + AND_CMD +
+                CD_CMD + simDir + AND_CMD +
+                configureX86 + AND_CMD +
+                CD_CMD + deviceDir + AND_CMD +
+                configureARM;
+            
+            // Copy and run configure-qsk
+            _exec(cmd, function (success) {
+                if (!success) {
+                    callback(false, "Failed executing command: " + cmd);
+                }
+
+                // Make for Simulator
+                _spawn(MAKE_CMD, simDir, function (success, err) {
+                    if (!success) {
+                        callback(false, err);
+                    }
+
+                    // Make for Device
+                    _spawn(MAKE_CMD, deviceDir, function (success, err) {
+                        if (!success) {
+                            callback(false, err);
+                        }
+                    
+                        cmd = CD_CMD + simDir + AND_CMD + stripX86 + AND_CMD +
+                            CD_CMD + deviceDir + AND_CMD + stripARM;
+                        
+                        // Strip .so files
+                        _exec(cmd, function (success) {
+                            if (!success) {
+                                callback(false, "Failed executing command: " + cmd);
+                            }
+                            
+                            next();
+                        });
+                    });
+                });
+            });
+        }
+    } else {
+        next();
     }
-    return cmd;
+}
+
+function _extHandler(callback) {
+    var exts = fs.readdirSync(_c.EXT),
+        index = -1,
+        next;
+
+    next = function () {
+        index++;
+
+        if (index === exts.length) {
+            callback(true);
+        }
+
+        _buildNative(exts[index], next, callback);
+    };
+
+    next();
 }
 
 module.exports = function (prev, baton) {
     baton.take();
 
-    childProcess.exec(_getCmd(), function (error, stdout, stderr) {
-        if (error) {
-            console.log(stdout);
-            console.log(stderr);
-            baton.pass(error.code);
+    _extHandler(function (success, err) {
+        if (!success) {
+            baton.drop(err);
         } else {
             baton.pass(prev);
         }
