5c7b52e82401a7f3570c0c3c0e8eb102a4c508c3
Rowell Cruz <rcruz@rim.com>
2012-06-20 14:24:18 -0400

    Added initial logic for listBuilder

--- ui-resources/plugins/listBuilder/index.js
+++ ui-resources/plugins/listBuilder/index.js
@@ -9,18 +9,44 @@ function init() {
     listItems = [];
 }
 
+function invokeApp(listItem) {
+    // invoke some app based on ID
+    console.log("invoking");
+    listBuilder.hide();
+}
+
+function handleMouseDown(evt) {
+    evt.preventDefault();
+}
+
 listBuilder = {
     init: init,
     populateList: function (itemArray) {
+        var listContent = document.getElementById('listContent'),
+            listItem,
+            item; 
+
         // create a bunch of subdivs
+        for (item in itemArray) {
+            listItem = document.createElement('div');
+            listItem.appendChild(document.createTextNode(itemArray[item].name));
+            listItem.setAttribute('class', 'listItem');
+            listItem.ontouchend = invokeApp.bind(this, listItem);
+            listItem.addEventListener('mousedown', handleMouseDown, false);
+            listContent.appendChild(listItem);
+        }
         // subdivs will have a click handler to run stuff
         // hide menu after handling a click
     },
     show: function () {
         // set css class to visible
+        var listContainer = document.getElementById('listContainer');
+        listContainer.setAttribute('class', 'showList');
     },
     hide: function () {
         // set css class to hidden
+        var listContainer = document.getElementById('listContainer');
+        listContainer.setAttribute('class', 'hideList');
     }
 };
 
